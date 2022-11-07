73b35097034d8f95f44c15c302bad96df29c6311
Dperdiaz <74120485+Dabycito@users.noreply.github.com>
2022-11-07 17:47:17 -0300

    Upd 07/11

--- parse.py
+++ parse.py
@@ -8,17 +8,22 @@ Tabla = ["Secuencia1","Secuencia2","Distancia"]
 #Largo de la tabla ser como len(df.iloc[0])-1
 
 TableLen = len(df.iloc[0])-1
-AuxMax = 2
-
+print(TableLen)
+AuxMax = 3
 
 for i in range(1,TableLen):
     for j in range(2,AuxMax):
         Tabla.append(str(df.columns[j]))
+        print(df.columns[j])
         Tabla.append(str(df.iloc[i][0]))
+        print(df.iloc[i][0])
         Tabla.append(str(df.iloc[i][j-1]))
-    AuxMax=+1
+        print(df.iloc[i][j-1])
+    AuxMax=AuxMax+1
     
 print(Tabla)
+
+
 
 '''print(df.columns[2]) #De 2 hasta MaxColumnas
 print(df.iloc[1][0])
@@ -51,8 +56,8 @@ print(df.iloc[2][2])
 print(df.iloc[3][1])
 print(df.iloc[3][2])
 print(df.iloc[3][3])
-
-print("El largo de la tabla es de "+str(len(df.iloc[0])-1))
 '''
+print("El largo de la tabla es de "+str(len(df.iloc[0])-1))
+
 
         
